8a8e0fd0fcb1ec44c701979c9c690a201da17682
Arthur Malmis <almalmis1393@gmail.com>
2018-05-17 10:58:23 -0400

    added custom routes for show

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do 
-	resources :portfolios
-	
-	get 'about', to: 'pages#about'
-	get 'contact', to: 'pages#contact'
+Rails.application.routes.draw do
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-	resources :blogs
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-	root to: 'pages#home'
+  resources :blogs
+  
+  root to: 'pages#home'
 end
